5ed879cb4334f1414e8b3aa11b22a62a2420fc6d
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:33:35 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,15 +2,15 @@ from conans import ConanFile, tools, os
 
 class BoostNumeric_IntervalConan(ConanFile):
     name = "Boost.Numeric_Interval"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-interval"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["interval"]
-    requires =  "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Detail/1.64.0@bincrafters/testing", \
-                      "Boost.Logic/1.64.0@bincrafters/testing"
+    requires =  "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Detail/1.65.1@bincrafters/testing", \
+                      "Boost.Logic/1.65.1@bincrafters/testing"
 
                       #config0 detail5 logic3
 
