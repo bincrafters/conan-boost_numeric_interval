88cda7cbac60a8ef8b2a4a8a7f629d099a97de71
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:06 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,16 +4,10 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostNumeric_IntervalConan(base.BoostBaseConan):
     name = "boost_numeric_interval"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_numeric_interval"
-    lib_short_names = ["interval"]
-    header_only_libs = ["interval"]
-    b2_requires = [
-        "boost_config",
-        "boost_detail",
-        "boost_logic"
-    ]
+    version = "1.70.0"
+    boost_source_repo = {'numeric_interval': "interval"}
